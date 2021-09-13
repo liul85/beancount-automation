5e2ab4d7af17942c99ce06eb0a5e80fb6b44700b
liul85 <liul85@gmail.com>
2021-09-13 20:35:18 +1000

    log request for debugging

--- api/beancount.rs
+++ api/beancount.rs
@@ -1,12 +1,17 @@
 use http::StatusCode;
+use log::info;
 use std::error::Error;
 use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
     Ok(lambda!(handler))
 }
 
-fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
+fn handler(request: Request) -> Result<impl IntoResponse, VercelError> {
+    let body = String::from_utf8_lossy(request.body());
+    info!("request body is {}", body);
+
     let response = Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/plain")
