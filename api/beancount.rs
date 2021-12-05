229a85d658349fc30226859ce32f79cfc8a4cbf8
liul85 <liul85@gmail.com>
2021-12-05 23:33:55 +1100

    Refactor a bit

--- api/beancount.rs
+++ api/beancount.rs
@@ -19,7 +19,7 @@ fn handler(request: Request) -> Result<impl IntoResponse, VercelError> {
     info!("request body is {}", body);
 
     let update: Update =
-        serde_json::from_str(&body).or_else(|e| Err(VercelError::new(e.to_string().as_str())))?;
+        serde_json::from_str(&body).map_err(|e| VercelError::new(e.to_string().as_str()))?;
 
     let parser = Parser::new().or_else(|e| {
         Err(VercelError::new(
@@ -27,45 +27,40 @@ fn handler(request: Request) -> Result<impl IntoResponse, VercelError> {
         ))
     })?;
 
+    let ok_response = |text| {
+        let response_body = ResponseBody {
+            method: "sendMessage".into(),
+            chat_id: update.message.chat.id,
+            text,
+            reply_to_message_id: update.message.message_id,
+        };
+
+        Ok(Response::builder()
+            .status(StatusCode::OK)
+            .header("Content-Type", "application/json")
+            .body(serde_json::to_string(&response_body).unwrap())?)
+    };
+
     let transaction = match parser.parse(&update.message.text) {
         Ok(transaction) => transaction,
         Err(e) => {
             error!("Failed to parse input: {}", e.to_string());
-            let response_body = ResponseBody {
-                method: "sendMessage".into(),
-                chat_id: update.message.chat.id,
-                text: format!("⚠️\n==============================\nFailed to parse input: {}", e.to_string()),
-                reply_to_message_id: update.message.message_id,
-            };
-            return Ok(Response::builder()
-                .status(StatusCode::OK)
-                .header("Content-Type", "application/json")
-                .body(serde_json::to_string(&response_body).unwrap())?);
+            return ok_response(format!(
+                "⚠️\n==============================\nFailed to parse input: {}",
+                e.to_string()
+            ));
         }
     };
 
     info!("parsed transaction is {:?}", transaction);
 
-    let store = GithubStore::new().or_else(|e| {
-        Err(VercelError::new(
-            format!("Failed to create github store: {}", e).as_str(),
-        ))
-    })?;
+    let store = GithubStore::new()
+        .map_err(|e| VercelError::new(format!("Failed to create github store: {}", e).as_str()))?;
 
     match store.save(transaction.to_beancount()) {
         Ok(_) => {
             info!("Successfully saved transaction!");
-            let response_body = ResponseBody {
-                method: "sendMessage".into(),
-                chat_id: update.message.chat.id,
-                text: transaction.into(),
-                reply_to_message_id: update.message.message_id,
-            };
-
-            Ok(Response::builder()
-                .status(StatusCode::OK)
-                .header("Content-Type", "application/json")
-                .body(serde_json::to_string(&response_body).unwrap())?)
+            ok_response(transaction.into())
         }
         Err(e) => {
             error!("Failed to save transaction: {}", e.to_string());
