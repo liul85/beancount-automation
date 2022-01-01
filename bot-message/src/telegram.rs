ad723f218468b01db7bc4f68d042d52427fd6d04
liul85 <liul85@gmail.com>
2022-01-01 11:19:56 +1100

    Fixed tests and added one more

--- bot-message/src/telegram.rs
+++ bot-message/src/telegram.rs
@@ -46,10 +46,25 @@ pub struct ResponseBody {
 mod tests {
     use super::*;
     #[test]
-    fn it_deserialize_update() {
+    fn it_deserialize_update_with_message() {
         let json = "{\"update_id\":459592837, \"message\":{\"message_id\":7,\"from\":{\"id\":247673932,\"is_bot\":false,\"first_name\":\"Liang\",\"username\":\"liul85\",\"language_code\":\"en\"},\"chat\":{\"id\":247673932,\"first_name\":\"Liang\",\"username\":\"liul85\",\"type\":\"private\"},\"date\":1631506802,\"text\":\"@KFC chicken 12.9 AUD CBA > food\"}}";
         let update: Update = serde_json::from_str(&json).unwrap();
         assert_eq!(update.update_id, 459592837);
-        assert_eq!(update.message.text, "@KFC chicken 12.9 AUD CBA > food");
+        assert_eq!(
+            update.message.unwrap().text,
+            "@KFC chicken 12.9 AUD CBA > food"
+        );
+    }
+
+    #[test]
+    fn it_deserialize_update_with_edited_message() {
+        let json = "{\"update_id\":459593047,\"edited_message\":{\"message_id\":276,\"from\":{\"id\":247673932,\"is_bot\":false,\"first_name\":\"Liang\",\"username\":\"liul85\",\"language_code\":\"en\"},\"chat\":{\"id\":247673932,\"first_name\":\"Liang\",\"username\":\"liul85\",\"type\":\"private\"},\"date\":1640933453,\"edit_date\":1640933464,\"text\":\"2021-12-30 @Coles 30 cba > food\",\"entities\":[{\"offset\":11,\"length\":6,\"type\":\"mention\"}]}}";
+        let update: Update = serde_json::from_str(&json).unwrap();
+        assert_eq!(update.update_id, 459593047);
+        assert!(update.message.is_none());
+        assert_eq!(
+            update.edited_message.unwrap().text,
+            "2021-12-30 @Coles 30 cba > food"
+        );
     }
 }
